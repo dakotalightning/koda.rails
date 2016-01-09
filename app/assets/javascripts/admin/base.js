5236e8225527980d3f4f990af8714ae5df7e93cd
Dakota Lightning <im@dakotalightning.com>
2016-01-09 01:59:09 -0800

    precompile js, css for admin

---
+++ app/assets/javascripts/admin/base.js
@@ -0,0 +1,38 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require jquery.turbolinks
+//= require turbolinks
+//= require sweetalert
+//= require highlightjs
+//= require marked
+//= require sweet-alert-confirm
+//= require bootstrap-sprockets
+
+$(document).on('ready page:load', function() {
+  marked.setOptions({
+    gfm: true,
+    tables: true,
+    pedantic: false,
+    sanitize: false,
+    smartLists: true,
+    smartypants: false,
+    langPrefix: 'lang-',
+    highlight: function (code) {
+        return hljs.highlightAuto(code).value;
+    }
+  });
+
+  document.getElementById('markdown').innerHTML = document.getElementById('page_content').innerHTML);
+});
