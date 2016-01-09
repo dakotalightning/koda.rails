11d5c0c26296742b8e26fc1458088631bd801866
Dakota Lightning <im@dakotalightning.com>
2016-01-09 01:42:07 -0800

    pages render markdown

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,9 @@
 class PagesController < ApplicationController
 
   def show
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
     @page = Page.find_by(permalink: params[:page])
+    @content = markdown.render(@page.content)
   end
-  
+
 end
